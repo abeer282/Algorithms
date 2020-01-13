3cf1d3a31fc20f8ffba010ad9d37f8a1a889aba3
abeer282 <abeerala282@gmail.com>
2020-01-13 23:50:37 +0200

    added empty sha1 function

--- SHA-1/Source.c
+++ SHA-1/Source.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+char* SHA1(char* plain) {
+	return plain;
+}
 int main() {
+	int i = 0;
+	char* myPlainText = (char*)malloc(sizeof(char)*11);
+	char*myHash;
 
+	for (; i < 10; *(myPlainText + i) = 'a' + i, i++);//myPlainText=abcdefghij
+	*(myPlainText + i) = '\0';
+
+
+	printf("Plain: %s\n", myPlainText);
+	myHash = SHA1(myPlainText);
+	printf("Hash: %s\n", myPlainText);
+
+	getchar();
 	return 0;
 }
