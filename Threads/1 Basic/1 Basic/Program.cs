030afe6964826f4f63d3fe78e3b0b2dc8ff7a0e3
abeer282 <abeerala282@gmail.com>
2020-06-02 20:12:14 +0300

    Add basic threading

---
+++ Threads/1 Basic/1 Basic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace _1_Basic
+{
+    class Program
+    {
+
+        public static void Server()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Secondary: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(Server));
+            thread.Start();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Main: Do stuff number {0}.",i);
+                Thread.Sleep(0);
+            }
+            Console.ReadLine();
+        }
+    }
+}
