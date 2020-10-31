0d9b784249ab2286101822f7889b940ebdfc4a72
Abeer <56076898+abeer282@users.noreply.github.com>
2020-10-31 22:18:22 +0200

    Custom objects

--- Pro JaveScript/object oriented JS/objects.js
+++ Pro JaveScript/object oriented JS/objects.js
@@ -1,1 +1,18 @@
 /***custom objects***/
+
+//literal notation 
+var system = {
+    users: 3,
+    sharedFiles: false,
+    boot: function() {},
+    shutdown: function() {}
+};
+
+//reading val
+system.users
+//calling func
+system.boot()
+//updating val
+system.users = 4
+//adding property
+system.admins = 2
