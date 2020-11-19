bc8797a8b3656fabc4c3bd4ccfa502c5bf7feb9b
Abeer <56076898+abeer282@users.noreply.github.com>
2020-11-19 19:14:44 +0200

    inheritance

--- Pro JaveScript/object oriented JS/objects_cont.js
+++ Pro JaveScript/object oriented JS/objects_cont.js
@@ -12,3 +12,37 @@ Corona.prototype.vaccine = function() {
 var cor = new Corona();
 cor.infection().vaccine().isSick //false
 
+
+/******* inheritance ********/
+
+//class
+function Accommodation() {}
+//methods of Accom class:
+Accommodation.prototype.lock = function() {};
+Accommodation.prototype.unlock = function() {};
+
+//constructor that will be a subclass
+function House(defaults) {
+    defaults = defaults || {};
+    this.floors = 2;
+    this.rooms = defaults.rooms || 7;
+}
+//house class will inherit everything from Accom
+House.prototype = new Accommodation();
+//setting House to be the constructor of House else each init of House class will be considered instance of Accom
+House.prototype.constructor = House; 
+
+var myHouse = new House(); 
+var myNeighborsHouse = new House({
+    rooms: 8
+});
+
+myHouse.rooms // 7
+myNeighborsHouse.rooms // 8
+
+myHouse.lock();
+myNeighborsHouse.unlock();
+myHouse.constructor === House // true 
+myHouse.constructor === Accommodation // false
+myNeighborsHouse instanceof House // true
+myNeighborsHouse instanceof Accommodation // true
