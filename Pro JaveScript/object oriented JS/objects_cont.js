1e84fd64d01a0347fe62c1e2e9a2ef7cf5f07af6
Abeer <56076898+abeer282@users.noreply.github.com>
2020-11-19 18:07:55 +0200

    chaining methods

--- Pro JaveScript/object oriented JS/objects_cont.js
+++ Pro JaveScript/object oriented JS/objects_cont.js
@@ -1,1 +1,14 @@
+//chaining methods
+
+function Corona() {}
+Corona.prototype.infection = function() {
+    this.isSick = true;
+    return this;
+}
+Corona.prototype.vaccine = function() {
+    this.isSick = false;
+    return this;
+}
+var cor = new Corona();
+cor.infection().vaccine().isSick //false
 
